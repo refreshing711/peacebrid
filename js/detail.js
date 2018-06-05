fbb5744428fdc36af6f5526db31645f37d9bb1dd
refreshing711 <1529067310@qq.com>
2018-06-05 23:00:08 +0800

    完成了购物车的添加页面，购物车静态页面，详情页待完善

--- js/detail.js
+++ js/detail.js
@@ -96,8 +96,203 @@ $('#list img').each(function() {
 })
 
 
+$(function(){
+	loadCart();
+	
+	$("#buy").click(function(){
+	//	location.href="shopcart.html";
+	})
+	
+	$(".good .add .addToCart").click(function(e){
+		//console.log($(this).parents());
+		var goodId=$(this).parents(".detail-main").attr("data-good-id");
+	//	console.log(goodId);
+		var goodSrc=$(this).parents(".glass-left").find(".shopimg").attr("src");
+	//	console.log(goodSrc);
+		var goodName=$(this).parent().siblings("h3").html();
+	//	console.log(goodName);
+		
+		var goodSerial=$(this).parents(".detail-main").find(".shopnum").html();
+		
+	//	console.log(goodSerial);
+		
+//		console.log($(this).parent().siblings(".price").children("span"))
+		
+		var goodMagic=$(this).parent().siblings(".price").children("span").children("b").html();
+//	console.log(goodMagic);
+	
+	
+	
+	var goodColor=$(this).parent().siblings(".p2").children("a").attr("title");
+//	console.log(goodColor);
+	
+	
+	
+	var goodSize=$(this).parent().siblings(".p3").children("a").html();
+//	console.log(goodSize);
+	
+	
+	var goodNum=$(this).parent().siblings(".p4").children().eq(2).val();
+//	console.log(goodNum);
+	var goodShoppe=$(this).parent().siblings(".price").children("p").children("del").html();
+//	console.log(goodShoppe);
+	
+	
+	var goodPrice=$(this).parent().siblings(".price").children("ul").children().eq(1).html();
+//	console.log(goodPrice);
+	
+	var cartStr=$.cookie("cart")?$.cookie("cart"):"";
+	var cartObj=convertCartStrToObj(cartStr);
+	
+//	console.log(cartObj)
+		if(goodId in cartObj){
+			cartObj[goodId].num+=1;
+		}else{
+			cartObj[goodId]={
+				src:goodSrc,
+				name:goodName,
+				serial:goodSerial,
+				magic:goodMagic,
+				color:goodColor,
+				size:goodSize,
+				num:goodNum,
+				shoppe:goodShoppe,
+				price:goodPrice
+			};
+		}
+		//购物车信息存回cookie
+		
+		cartStr=convertObjToCartStr(cartObj);
+		
+//		存入cookie
+
+		$.cookie("cart",cartStr,{expires:7,path:"/"});
+		
+		
+	//	购物车（0）
+//		var number=$("#buy").val();
+//		
+//		console.log(number)
+//	
+
+//		(function(){
+//		var n=	$("#buy").val()
+//		var num=parseInt(n);
+//		return num+1
+//		})()
+//		
+//		console.log(num)
+		
+		var cloneImg = $(this).parents(".glass-left").find(".shopimg").clone().css({width:50,height:50});
+					cloneImg.fly({
+						start : {
+							top : e.clientY,
+							left : e.clientX
+						},
+						end :{
+							top : $("#buy").offset().top,
+							left : $("#buy").offset().left,
+							width:0,
+							height:0
+						},
+						autoPlay : true,
+						onEnd : function(){
+							$("#buy").val(function(index,v){
+						//"购物车（0）"
+						var pattern = /(\d+)/;
+						var num = parseInt(v.match(pattern)[1]);
+						return "购物车(" + (num + 1) + ")";
+					});
+					cloneImg.remove();
+						}
+					})
+					
+					
+					
+					
+		
+	})
+});
 
 
+		function convertCartStrToObj(cartStr){
+			if(!cartStr){
+				return{};
+			}
+			var goods=cartStr.split(":");
+			var obj={};
+			for(var i=0;i<goods.length;i++){
+				var data=goods[i].split(",");
+				
+				obj[data[0]]={
+					src:data[1],
+					name:data[2],
+					serial:data[3],
+					magic:data[4],
+					color:data[5],
+					size:data[6],
+					num:data[7],
+					shoppe:data[8],
+					price:data[9]
+				}
+			}
+			return obj;
+		}
+		function convertObjToCartStr(obj){
+			
+			var cartStr="";
+			for(var id in obj){
+				if(cartStr){
+					cartStr+=":";
+				}
+				
+				cartStr+=id+","+
+	obj[id].src+","+obj[id].name+","+obj[id].serial+","+obj[id].magic+","+obj[id].color+","+obj[id].size+","+obj[id].num+","+obj[id].shoppe+","+obj[id].price;
+					
+			}
+			return cartStr;
+		}
+		
+	function loadCart(){
+		var cartStr=$.cookie("cart")?$.cookie("cart"):"";
+		var cartObj=convertCartStrToObj(cartStr);
+		
+		var total=0;
+		for(var id in cartObj){
+			total+=cartObj[id].num;
+			
+		}
+		
+		$("#buy").val("购物车(" + total + ")");
+		//$("#buy").val(total)
+	}
 
+		
+		
+		
+
+		
+	
+	$(".glass-right .i1").click(function(){
+			$(this).parent().siblings("ul").children().eq(0).animate({
+				"top":554
+			})
+			$(this).parent().siblings("ul").children().eq(1).animate({
+				"top":830
+			})
+			$(this).parent().siblings("ul").children().eq(2).animate({
+				"top":0
+			})
+			$(this).parent().siblings("ul").children().eq(3).animate({
+				"top":276
+			})
+	})
+	$(".glass-right .i2").click(function(){
+//			$(this).parent().siblings("ul").children().animate({
+//				"top":0
+//			})
+	})
+	
+	
 
 }
